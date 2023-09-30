39923d7ee2dbf0785dcff15a3c9e121e1177e8bd
k-murakami0609 <dadgac@gmail.com>
2023-09-30 23:57:25 +0900

    Page を実装した

---
+++ app/file/Page.rb
@@ -0,0 +1,55 @@
+require 'stringio'
+
+class Page
+  CHARSET = Encoding::UTF_8
+  BYTES_PER_UNIT = 4
+
+  def initialize(arg)
+    case arg
+    when Integer # blocksize
+      @buffer = StringIO.new("\0" * arg)
+    when String # byte array
+      @buffer = StringIO.new(arg)
+    else
+      raise ArgumentError, "Expected Integer or String, got #{arg.class}"
+    end
+  end
+
+  def get_int(offset)
+    @buffer.seek(offset)
+    @buffer.read(BYTES_PER_UNIT).unpack1('l>')
+  end
+
+  def set_int(offset, n)
+    @buffer.seek(offset)
+    @buffer.write([n].pack('l>'))
+  end
+
+  def get_bytes(offset)
+    @buffer.seek(offset)
+    length = @buffer.read(BYTES_PER_UNIT).unpack1('l>')
+    @buffer.read(length).bytes
+  end
+
+  def set_bytes(offset, bytes)
+    @buffer.seek(offset)
+    @buffer.write([bytes.length].pack('l>'))
+    @buffer.write(bytes.pack('c*'))
+  end
+
+  def get_string(offset)
+    get_bytes(offset).pack('c*').force_encoding(CHARSET)
+  end
+
+  def set_string(offset, str)
+    set_bytes(offset, str.encode(CHARSET).bytes)
+  end
+
+  def self.max_length(str)
+    4 + str.length * BYTES_PER_UNIT
+  end
+
+  def contents
+    @buffer.string
+  end
+end
---
+++ test/file/page_test.rb
@@ -0,0 +1,63 @@
+require 'minitest/autorun'
+require_relative '../../app/file/Page'
+
+class PageTest < Minitest::Test
+  def setup
+    # Do nothing
+  end
+
+  def teardown
+    # Do nothing
+  end
+
+  def test_get_and_set_int
+    page = Page.new(100)
+    page.set_int(0, 100)
+    page.set_int(4, 200)
+    page.set_int(8, 300)
+
+    assert_equal 100, page.get_int(0)
+    assert_equal 200, page.get_int(4)
+    assert_equal 300, page.get_int(8)
+  end
+
+  def test_get_and_set_string
+    page = Page.new(100)
+    offset = 0
+
+    test_strings = ['hello', '🀄🀄🀄', 'ruby は便利']
+
+    test_strings.each do |str|
+      page.set_string(offset, str)
+      offset += Page.max_length(str)
+    end
+
+    offset = 0
+    test_strings.each do |str|
+      assert_equal str, page.get_string(offset)
+      offset += Page.max_length(str)
+    end
+  end
+
+  def test_get_and_set_bytes
+    page = Page.new(100)
+    offset = 0
+
+    test_bytes_arrays = [
+      [0, 1, 2, 3, 4, 5, 6],
+      [6, 7, 8, 9],
+      [0x23, 0x44]
+    ]
+
+    test_bytes_arrays.each do |bytes_array|
+      page.set_bytes(offset, bytes_array)
+      offset += Page.max_length(bytes_array)
+    end
+
+    offset = 0
+    test_bytes_arrays.each do |bytes_array|
+      assert_equal bytes_array, page.get_bytes(offset)
+      offset += Page.max_length(bytes_array)
+    end
+  end
+end
