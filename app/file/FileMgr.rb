62e7551c49864f052949046299f85132d87c9ae3
k-murakami0609 <dadgac@gmail.com>
2023-10-01 01:18:33 +0900

    FIleMgrを追加した

---
+++ app/file/FileMgr.rb
@@ -0,0 +1,85 @@
+# frozen_string_literal: true
+
+require 'fileutils'
+require_relative 'BlockId'
+
+# 階層構造
+# File
+# ├───Block
+# │   ├───Page
+# │   │   ├───Record
+# │   │   └───Record
+# │   └───Page
+# │       └───Record
+# └───Block
+#     ├───Page
+#     │   └───Record
+#     └───Page
+#         └───Record
+class FileMgr
+  attr_reader :block_size, :is_new
+
+  def initialize(db_directory, block_size)
+    @db_directory = db_directory
+    @block_size = block_size
+    @is_new = !Dir.exist?(db_directory)
+
+    # create the directory if the database is new
+    FileUtils.mkdir_p(db_directory) if @is_new
+
+    # remove any leftover temporary tables
+    Dir.each_child(db_directory) do |filename|
+      File.delete(File.join(db_directory, filename)) if filename.start_with?('temp')
+    end
+
+    @open_files = {}
+  end
+
+  def read(block_id, page)
+    f = get_file(block_id.filename)
+    f.seek(block_id.blknum * @block_size)
+    f.read(page.contents.size, page.contents)
+  rescue StandardError => e
+    raise "cannot read block #{block_id}: #{e.message}"
+  end
+
+  def write(block_id, page)
+    f = get_file(block_id.filename)
+    f.seek(block_id.blknum * @block_size)
+    f.write(page.contents)
+  rescue StandardError => e
+    raise "cannot write block #{block_id}: #{e.message}"
+  end
+
+  def append(filename)
+    new_block_num = length(filename)
+    block_id = BlockId.new(filename, new_block_num)
+    begin
+      f = get_file(block_id.filename)
+      f.seek(block_id.blknum * @block_size)
+      f.write("\0" * @block_size)
+    rescue StandardError => e
+      raise "cannot append block #{block_id}: #{e.message}"
+    end
+    block_id
+  end
+
+  def length(filename)
+    f = get_file(filename)
+    f.size / @block_size
+  rescue StandardError => e
+    raise "cannot access #{filename}: #{e.message}"
+  end
+
+  private
+
+  def get_file(filename)
+    f = @open_files[filename]
+    unless f
+      db_table = File.join(@db_directory, filename)
+      f = File.new(db_table, 'w+b')
+      @open_files[filename] = f
+    end
+    f
+  end
+end
---
+++ test/file/file_mgr_test.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative '../../app/file/FileMgr'
+require_relative '../../app/file/Page'
+require_relative '../../app/file/BlockId'
+
+class FileMgrTest < Minitest::Test
+  def setup
+    # Do nothing
+  end
+
+  def teardown
+    # Do nothing
+  end
+
+  def test_can_write_and_read_in_multiple_blocks
+    file_mgr = FileMgr.new('./tmp/file_manager', 100)
+    block_id1 = BlockId.new('can_write_and_read_in_multiple_blocks1', 0)
+    block_id2 = BlockId.new('can_write_and_read_in_multiple_blocks2', 1)
+
+    write_page1 = Page.new(100)
+    write_page2 = Page.new(100)
+
+    write_page1.set_int(0, 100)
+    write_page1.set_int(4, 200)
+    write_page1.set_int(8, 300)
+    write_page1.set_string(12, '🀄🀄🀄')
+    write_page2.set_string(0, 'hello')
+
+    file_mgr.write(block_id1, write_page1)
+    file_mgr.write(block_id2, write_page2)
+
+    read_page1 = Page.new(100)
+    read_page2 = Page.new(100)
+
+    file_mgr.read(block_id1, read_page1)
+    file_mgr.read(block_id2, read_page2)
+
+    assert_equal 200, read_page1.get_int(4)
+    assert_equal '🀄🀄🀄', read_page1.get_string(12)
+    assert_equal 'hello', read_page2.get_string(0)
+  end
+
+  def test_counts_blocks
+    file_mgr = FileMgr.new('./tmp/file_manager', 100)
+
+    block_id1 = BlockId.new('count_blocks', 0)
+    block_id2 = BlockId.new('count_blocks', 1)
+    block_id3 = BlockId.new('count_blocks', 2)
+
+    page = Page.new(100)
+
+    file_mgr.write(block_id1, page)
+    file_mgr.write(block_id2, page)
+    file_mgr.write(block_id3, page)
+
+    assert_equal file_mgr.length('count_blocks'), 3
+  end
+
+  def test_append
+    file_mgr = FileMgr.new('./tmp/file_manager', 100)
+
+    file_mgr.append('append_new_block')
+    file_mgr.append('append_new_block')
+    file_mgr.append('append_new_block')
+    file_mgr.append('append_new_block')
+
+    assert_equal file_mgr.length('append_new_block'), 4
+  end
+end
