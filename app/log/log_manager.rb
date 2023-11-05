e569babf4b1ffb60d4dccdf37918f46512ca01d5
k-murakami0609 <dadgac@gmail.com>
2023-11-05 18:19:04 +0900

    fix

--- app/log/log_manager.rb
+++ app/log/log_manager.rb
@@ -4,20 +4,20 @@ require_relative "../file/block_id"
 require_relative "../file/page"
 require_relative "log_iterator"
 class LogManager
-  attr_accessor :file_manager, :logfile, :logpage, :current_block_id, :latest_lsn, :last_saved_lsn
+  attr_accessor :file_manager, :log_file, :log_page, :current_block_id, :latest_lsn, :last_saved_lsn
 
   INTEGER_SIZE = 4
   def initialize(file_manager, logfile)
     @file_manager = file_manager
-    @logfile = logfile
-    @logpage = Page.new(file_manager.block_size)
+    @log_file = logfile
+    @log_page = Page.new(file_manager.block_size)
     log_size = file_manager.length(logfile)
 
     @current_block_id = if log_size.zero?
       append_new_block
     else
       BlockId.new(logfile, log_size - 1)
-      file_manager.read(@current_block_id, @logpage)
+      file_manager.read(@current_block_id, @log_page)
     end
     @latest_lsn = 0
     @last_saved_lsn = 0
@@ -26,7 +26,7 @@ class LogManager
   def flush(lsn = nil)
     return unless lsn.nil? || lsn >= @last_saved_lsn
 
-    @file_manager.write(@current_block_id, @logpage)
+    @file_manager.write(@current_block_id, @log_page)
     @last_saved_lsn = @latest_lsn
   end
 
@@ -35,30 +35,29 @@ class LogManager
     LogIterator.new(@file_manager, @current_block_id)
   end
 
-  def append(logrec)
-    boundary = @logpage.get_int(0)
-    rec_size = logrec.length
-    bytes_needed = rec_size + INTEGER_SIZE
+  def append(log_record)
+    boundary = @log_page.get_int(0)
+    record_size = log_record.length
+    bytes_needed = record_size + INTEGER_SIZE
 
     if boundary - bytes_needed < INTEGER_SIZE
       flush
       @current_block_id = append_new_block
-      boundary = @logpage.get_int(0)
+      boundary = @log_page.get_int(0)
     end
 
     rec_pos = boundary - bytes_needed
-    @logpage.set_bytes(rec_pos, logrec)
-    @logpage.set_int(0, rec_pos)
+    @log_page.set_bytes(rec_pos, log_record)
+    @log_page.set_int(0, rec_pos)
     @latest_lsn += 1
-    @latest_lsn
   end
 
   private
 
   def append_new_block
-    block_id = @file_manager.append(@logfile)
-    @logpage.set_int(0, @file_manager.block_size)
-    @file_manager.write(block_id, @logpage)
+    block_id = @file_manager.append(@log_file)
+    @log_page.set_int(0, @file_manager.block_size)
+    @file_manager.write(block_id, @log_page)
     block_id
   end
 end
