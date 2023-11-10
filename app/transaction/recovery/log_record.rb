ff90e8ef217dba12b96995f8fa5e4dd67038d8cd
k-murakami0609 <dadgac@gmail.com>
2023-11-10 20:10:28 +0900

    rollback

--- app/transaction/recovery/log_record.rb
+++ app/transaction/recovery/log_record.rb
@@ -2,6 +2,7 @@
 
 require_relative "set_int_record"
 require_relative "set_string_record"
+require_relative "rollback_record"
 require_relative "operation_code"
 
 class LogRecord
@@ -12,10 +13,10 @@ class LogRecord
     page = Page.new(bytes)
 
     record_class = {
-      # CHECKPOINT => CheckpointRecord,
-      # START => StartRecord,
-      # COMMIT => CommitRecord,
-      # ROLLBACK => RollbackRecord,
+      # OperationCode::CHECKPOINT => CheckpointRecord,
+      # OperationCode::START => StartRecord,
+      # OperationCode::COMMIT => CommitRecord,
+      OperationCode::ROLLBACK => RollbackRecord,
       OperationCode::SET_INT => SetIntRecord,
       OperationCode::SET_STRING => SetStringRecord
     }[LogRecord.get_operation_code(page)]
