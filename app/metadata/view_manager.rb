d47a3360d9d1652c1efffa805057aa2b9447890a
k-murakami0609 <dadgac@gmail.com>
2023-11-17 22:04:28 +0900

    manager x 3

---
+++ app/metadata/view_manager.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+class ViewManager
+  MAX_VIEW_DEF = 100
+
+  attr_accessor :table_manager
+
+  def initialize(is_new, table_manager, transaction)
+    @table_manager = table_manager
+    if is_new
+      schema = Schema.new
+      schema.add_string_field("viewName", TableManager::MAX_NAME)
+      schema.add_string_field("viewDef", MAX_VIEW_DEF)
+      @table_manager.create_table("viewcat", schema, transaction)
+    end
+  end
+
+  def create_view(view_name, view_def, transaction)
+    layout = @table_manager.get_layout("viewCatalog", transaction)
+    table_scan = TableScan.new(transaction, "viewCatalog", layout)
+    table_scan.insert
+    table_scan.set_string("viewName", view_name)
+    table_scan.set_string("viewDef", view_def)
+    table_scan.close
+  end
+
+  def get_view_def(view_name, transaction)
+    result = nil
+    layout = @table_manager.get_layout("viewCatalog", transaction)
+    table_scan = TableScan.new(transaction, "viewCatalog", layout)
+    while table_scan.next?
+      if table_scan.get_string("viewName") == view_name
+        result = table_scan.get_string("viewDef")
+        break
+      end
+    end
+    table_scan.close
+    result
+  end
+end
