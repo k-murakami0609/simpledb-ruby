cf653ef3b89fd3f97cbbac897c93715fd38107ec
k-murakami0609 <dadgac@gmail.com>
2023-11-23 22:16:36 +0900

    えい

--- app/query/expression.rb
+++ app/query/expression.rb
@@ -1,43 +1,33 @@
 # frozen_string_literal: true
 
 class Expression
-  attr_accessor :value, :field_name
+  attr_accessor :value
 
   def initialize(value_or_field)
-    if value_or_field.is_a?(Constant)
-      @value = value_or_field
-      @field_name = nil
-    else
-      @value = nil
-      @field_name = value_or_field
-    end
+    @value = value_or_field
   end
 
   def evaluate(scan)
-    value || scan.get_value(field_name)
-  end
-
-  def field_name?
-    !field_name.nil?
-  end
-
-  def as_constant
-    value
-  end
-
-  def as_field_name
-    field_name
+    if @value.is_a? Constant
+      @value
+    else
+      scan.get_value(@value)
+    end
   end
 
   def applies_to?(schema)
-    value ? true : schema.has_field?(field_name)
+    if @value.is_a? Constant
+      true
+    else
+      schema.has_field?(@value)
+    end
   end
 
   def to_s
-    if value
-      value.to_s
+    if @value.is_a? Constant
+      @value.to_s
     else
-      field_name || ""
+      @value
     end
   end
 end
