d5046ad53ec8ede38e0fb79c98d5fa84a484a40b
k-murakami0609 <dadgac@gmail.com>
2023-11-27 14:54:36 +0900

    nit

--- test/log/log_iterator_test.rb
+++ test/log/log_iterator_test.rb
@@ -7,7 +7,7 @@ require_relative "../../app/server/simple_db"
 class LogIteratorTest < Minitest::Test
   def setup
     FileUtils.rm_rf("./tmp/log_iterator")
-    @simple_db = SimpleDB.new("./tmp/log_iterator", 30, 8)
+    @simple_db = SimpleDB.new("./tmp/log_iterator", 200, 8)
     @log_manager = @simple_db.log_manager
   end
 
@@ -17,19 +17,19 @@ class LogIteratorTest < Minitest::Test
 
   def test_iterator
     6.times do |i|
-      @log_manager.append([i, 2, 3, 4])
+      @log_manager.append([i, (2..100).to_a].flatten)
     end
 
     iter = @log_manager.iterator
 
-    assert_next_bytes(iter, [5, 2, 3, 4], 1)
-    assert_next_bytes(iter, [4, 2, 3, 4], 1)
-    assert_next_bytes(iter, [3, 2, 3, 4], 1)
-    assert_next_bytes(iter, [2, 2, 3, 4], 0)
-    assert_next_bytes(iter, [1, 2, 3, 4], 0)
-    assert_next_bytes(iter, [0, 2, 3, 4], 0)
+    assert_next_bytes(iter, [5, (2..100).to_a].flatten, 48)
+    assert_next_bytes(iter, [4, (2..100).to_a].flatten, 47)
+    assert_next_bytes(iter, [3, (2..100).to_a].flatten, 46)
+    assert_next_bytes(iter, [2, (2..100).to_a].flatten, 45)
+    assert_next_bytes(iter, [1, (2..100).to_a].flatten, 44)
+    assert_next_bytes(iter, [0, (2..100).to_a].flatten, 43)
 
-    assert_equal(false, iter.next?)
+    assert_equal(true, iter.next?)
   end
 
   def assert_next_bytes(iter, expected_bytes, expected_block_number)
